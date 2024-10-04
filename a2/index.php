335cdef37ce0f5a9cb3a9b2f794997d1f227b13a
s3955172 <163797770+s3955172@users.noreply.github.com>
2024-10-04 12:52:29 +1000

    Update index.php

--- a2/index.php
+++ a2/index.php
@@ -1,7 +1,13 @@
-<?php include('includes/header.inc'); ?>
-<?php include('includes/nav.inc'); ?>
+<?php 
+include('db_connect.inc');
+include('header.inc');
+include('nav.inc');
+?>
+
 <main>
-    <h1>Welcome to Pets Victoria</h1>
-    <img src="images/main.jpg" alt="Adopt a Pet">
+    <h1 class="site-title">Pets Victoria</h1>
+    <h2>Welcome to Pet Adoption</h2>
+    <img src="main.jpg" alt="Adopt a Pet" class="main-image">
 </main>
-<?php include('includes/footer.inc'); ?>
+
+<?php include('footer.inc'); ?>
