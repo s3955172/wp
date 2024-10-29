329c5a1e26838b71f134944437b2a5e82ed267c8
s3955172 <163797770+s3955172@users.noreply.github.com>
2024-10-29 12:49:36 +1100

    Create index.php

---
+++ a3/index.php
@@ -0,0 +1,17 @@
+<?php
+include('includes/db_connect.inc');
+include('includes/header.inc');
+include('includes/nav.inc');
+?>
+
+<main>
+    <h1 class="site-title">Pets Victoria</h1>
+    <h2>Welcome to Pet Adoption</h2>
+    
+    <!-- Centered Image -->
+    <img src="images/main.jpg" alt="Adopt a Pet" class="main-image">
+
+    <p>Explore the pets we have available for adoption and learn how you can bring a new member to your family.</p>
+</main>
+
+<?php include('includes/footer.inc'); ?>
