80e6dc7536b225b3232f5973925f598a49a46ed3
s3955172 <163797770+s3955172@users.noreply.github.com>
2024-10-31 11:58:13 +1100

    Update add.php

--- a3/add.php
+++ a3/add.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-include 'db_connect.inc';
-include 'header.inc';
+include 'includes/db_connect.inc';
+include 'includes/header.inc';
 
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
