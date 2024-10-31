0c6dcec233e03260288cf8c48674f89ed96ecd3a
s3955172 <163797770+s3955172@users.noreply.github.com>
2024-10-31 13:15:30 +1100

    Update logout.php

--- a3/logout.php
+++ a3/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
-header("Location: index.php");
+session_unset(); // Unset all session variables
+session_destroy(); // Destroy the session
+header("Location: login.php"); // Redirect to login page
 exit;
 ?>
