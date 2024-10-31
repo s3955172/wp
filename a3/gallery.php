afac84b72c8cefc233eb4f7be210bc0daa843cf5
s3955172 <163797770+s3955172@users.noreply.github.com>
2024-10-31 11:39:59 +1100

    Update gallery.php

--- a3/gallery.php
+++ a3/gallery.php
@@ -3,27 +3,15 @@ session_start();
 include 'db_connect.inc';
 include 'header.inc';
 
-$types = $conn->query("SELECT DISTINCT type FROM pets");
+$result = $conn->query("SELECT * FROM pets");
+
 ?>
 
 <div class="container">
     <h2>Gallery</h2>
-
-    <select id="petTypeFilter" onchange="filterPets()">
-        <option value="all">All Types</option>
-        <?php while ($type = $types->fetch_assoc()): ?>
-            <option value="<?php echo htmlspecialchars($type['type']); ?>">
-                <?php echo htmlspecialchars($type['type']); ?>
-            </option>
-        <?php endwhile; ?>
-    </select>
-
-    <div id="petGallery">
-        <?php
-        $pets = $conn->query("SELECT * FROM pets");
-        while ($pet = $pets->fetch_assoc()):
-        ?>
-            <div class="pet-item" data-type="<?php echo htmlspecialchars($pet['type']); ?>">
+    <div class="gallery-container">
+        <?php while ($pet = $result->fetch_assoc()): ?>
+            <div class="pet-item">
                 <a href="details.php?id=<?php echo $pet['id']; ?>">
                     <img src="images/<?php echo htmlspecialchars($pet['image']); ?>" alt="<?php echo htmlspecialchars($pet['name']); ?>">
                     <p><?php echo htmlspecialchars($pet['name']); ?></p>
@@ -32,19 +20,5 @@ $types = $conn->query("SELECT DISTINCT type FROM pets");
         <?php endwhile; ?>
     </div>
 </div>
-
-<script>
-function filterPets() {
-    let selectedType = document.getElementById("petTypeFilter").value;
-    let pets = document.getElementsByClassName("pet-item");
-    for (let pet of pets) {
-        if (selectedType === "all" || pet.dataset.type === selectedType) {
-            pet.style.display = "block";
-        } else {
-            pet.style.display = "none";
-        }
-    }
-}
-</script>
 
 <?php include 'footer.inc'; ?>
